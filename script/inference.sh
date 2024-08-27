5e49540151d41ca021a9d059c906618cd1f8c79c
MathsCode <nthaxjm@126.com>
2024-08-27 11:58:12 +0800

    add SpecEE

--- script/inference.sh
+++ script/inference.sh
@@ -1,5 +1,5 @@
 if [ "$1" == "EE" ]; then 
-python early_exit/EEinference.py > ./results/EEinference.txt
+python AutoEE/EEinference.py > ./results/EEinference.txt
 fi
 if [ "$1" == "baseline" ]; then 
 python baseline/TD_inference.py > ./results/baseline.txt
